7ea5df6d2913ad5b9fc96d071dba159f4301f66a
Victor Laurentino <victorlaurentino7@gmail.com>
2021-06-08 16:00:06 -0300

    projeto nodejs

---
+++ projeto-node-typescript/dist/routes/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var routes = express_1.Router();
+routes.get("/", function (request, response) { return response.json({ message: "Hello World!" }); });
+exports.default = routes;
